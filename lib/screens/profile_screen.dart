e5554ba8fede3e44154751ea1fd0e1d71b4326b2
GwenP06 <phillipsgwennan@gmail.com>
2026-03-18 00:13:33 +0000

    profile screen
    basic, placeholder

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -7,11 +6,9 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
+      appBar: AppBar(title: const Text('Profile')),
       body: const Center(
-        child: Text('This is the profile screen.'),
+        child: Text('User Profile Info Here'),
       ),
     );
   }
