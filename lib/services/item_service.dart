3d4a36fe5483252747ea150105a68f57cf259e3c
reemsbusinessandwork <reemsbusinessandwork@gmail.com>
2026-04-17 12:30:22 +0000

    edited some of the names (they had space so it messed with the code)

--- lib/services/item_service.dart
+++ lib/services/item_service.dart
@@ -4,8 +4,8 @@ import '../models/item_model.dart';
 
 class ItemService {
   Future<List<Item>> getItems() async {
-    final String response = await rootBundle.loadString('assets/data/ items.json');
-    final data = await json.decode(response);
-    return (data['items'] as List).map((i) => Item.fromJson(i)).toList();
+    final String response = await rootBundle.loadString('assets/data/items.json');
+    final data = await json.decode(response) as List;
+    return data.map((i) => Item.fromJson(i)).toList();
   }
 }
